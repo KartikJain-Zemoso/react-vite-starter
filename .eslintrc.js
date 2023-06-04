85afb1908dd7fd4e51572035007b7f5a04c4a7dc
KartikJain-Zemoso <kartik.jain@zemosolabs.com>
2023-06-04 15:52:32 +0530

    setup-prettier/eslint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: { browser: true, es2021: true },
+  extends: "eslint-config-zemoso",
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  root: true,
+  rules: {
+    "no-unused-vars": "error",
+    "no-console": "warn",
+    "no-var": "warn",
+    camelcase: "off",
+    "array-callback-return": "off",
+    "object-curly-spacing": ["warn", "always"],
+    semi: "off",
+    "react-refresh/only-export-components": "warn",
+  },
+};
