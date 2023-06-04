ff04f8b24fabb45de9311f3c72108bd62056621b
KartikJain-Zemoso <kartik.jain@zemosolabs.com>
2023-06-04 18:28:10 +0530

    fix/testing-setup

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,9 @@ export default defineConfig({
     // you might want to disable it, if you don't have tests that rely on CSS
     // since parsing CSS is slow
     css: true,
+    coverage: {
+      provider: "c8",
+      reporter: ["clover", "lcov"],
+    },
   },
 });
