eae98d5bbd0adc2b8c063430008a264eb85a7adf
KartikJain-Zemoso <kartik.jain@zemosolabs.com>
2023-06-04 21:13:48 +0530

    test/scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ThemeWrapper from "./wrappers/ThemeWrapper";
 import { CssBaseline } from "@mui/material";
 import ErrorBoundary from "./wrappers/ErrorBoundary";
+import Icon from "./_components/atoms/Icon ";
 function App() {
   return (
     <>
@@ -12,7 +13,19 @@ function App() {
             <ErrorBoundary>
               <BrowserRouter>
                 <Routes>
-                  <Route index element={<h1>React Vite Starter</h1>} />
+                  <Route
+                    index
+                    element={
+                      <>
+                        <h1>
+                          {" "}
+                          <Icon src="/assets/icons/Logo.svg" />
+                          React Vite Starter
+                        </h1>
+                        <h2>{import.meta.env.VITE_SOME_KEY}</h2>
+                      </>
+                    }
+                  />
                 </Routes>
               </BrowserRouter>
             </ErrorBoundary>
--- src/_components/atoms/Button/button.test.tsx
+++ src/_components/atoms/Button/button.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import React from "react";
+
 import Button from "./index";
 
 test("Should Renders a Button", () => {
